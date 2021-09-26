67608373e792b700e785be51c887920cad4c8c41
vppro1396 <vpawde1396@gmail.com>
2021-09-26 18:46:08 +0530

    React app with Team page

--- src/main/java/com/vaibhav/ipldashboard/controller/TeamController.java
+++ src/main/java/com/vaibhav/ipldashboard/controller/TeamController.java
@@ -5,11 +5,13 @@ import com.vaibhav.ipldashboard.repository.MatchRepository;
 import com.vaibhav.ipldashboard.repository.TeamRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TeamController {
     
     @Autowired
